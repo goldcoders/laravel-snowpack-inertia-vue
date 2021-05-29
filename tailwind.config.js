96a1af6f5955e6caf50693b7a1a2f5ee7f698332
Goldcoders <goldcoders@protonmail.com>
2021-05-29 11:21:20 +0800

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    darkMode: false,
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+    // specify other options here
+};
