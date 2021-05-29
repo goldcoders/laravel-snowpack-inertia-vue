91056ae1fd56c869df63833217290413f10be743
Goldcoders <goldcoders@protonmail.com>
2021-05-29 13:16:00 +0800

    remove mounting css

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,20 +1,15 @@
-// Snowpack Configuration File
-// See all supported options: https://www.snowpack.dev/reference/configuration
-
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
       "resources/js": { url: '/dist/js' },
-      "resources/css": { url: '/dist/css' }
-    /* ... */
   },
   plugins: [
     '@snowpack/plugin-vue',
     '@snowpack/plugin-postcss',
-    /* ... */
+    '@snowpack/plugin-dotenv',
   ],
   alias: {
-    '@': './resources/js'
+    '@': './resources/js',
   },
   packageOptions: {
     /* ... */
@@ -22,11 +17,10 @@ module.exports = {
   devOptions: {
     hmr: true,// enable hot reload, make sure to add import.meta.hot in app.js
     tailwindConfig: './tailwind.config.js',
-    /* ... */
   },
   buildOptions: {
     out: 'public',// laravel css and js are serve in public folder
     clean: false, // warning setting this to true will delete other files in public folder
-    /* ... */
+    metaUrlPath: '_snowpack',
   },
 };
