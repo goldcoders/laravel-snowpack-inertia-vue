b06ed2c8f71341b6e5ad488e62ccc3af55739029
Goldcoders <goldcoders@protonmail.com>
2021-05-29 11:25:08 +0800

    add vue3 for inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-require('./bootstrap');
+
+import { App, plugin } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
+import { createApp, h } from 'vue'
+
+const el = document.getElementById('app')
+
+const vueApp = createApp({
+  render: () => h(App, {
+    initialPage: JSON.parse(el.dataset.page),
+    resolveComponent: name => import(`./Pages/${name}.vue.js`).then(module => module.default),
+  })
+}).use(plugin).mount(el)
+
+InertiaProgress.init()
+
+if (import.meta.hot) {
+  import.meta.hot.accept()
+  import.meta.hot.dispose(() => {
+      vueApp.$destroy(); });
