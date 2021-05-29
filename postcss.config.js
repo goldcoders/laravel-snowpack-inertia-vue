b7d7ffd0d1ad7c59f68d782147c0036739bba25b
Goldcoders <goldcoders@protonmail.com>
2021-05-29 13:15:32 +0800

    config for tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
 module.exports = {
     plugins: {
-      tailwindcss: {},
+      tailwindcss: { config: './tailwind.config.js' },
       // other plugins can go here, such as autoprefixer
       autoprefixer: {},
     },
