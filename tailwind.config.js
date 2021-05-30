7b1a11e265179b14641d825f6a8e4584ae4ec735
Goldcoders <goldcoders@protonmail.com>
2021-05-30 13:08:14 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
     mode: 'jit',
     purge:{
+      // preserveHtmlElements: false, // useful for markdown source files
       options: {
-        keyframes: true,
-        fontFace: true,
+        // keyframes: true,
+        // fontFace: true,
+        // safelist: ['add-here-new-class-not-to-purge'],
+        // blocklist: [/^debug-/],
       },
       content: [
         // './storage/framework/views/*.php',
