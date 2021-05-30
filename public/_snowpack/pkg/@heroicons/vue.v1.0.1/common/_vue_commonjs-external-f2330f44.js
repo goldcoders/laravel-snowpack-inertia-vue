6c3ee29614699c0b8af7bd8fb5ab7abf01929a15
Goldcoders <goldcoders@protonmail.com>
2021-05-30 16:21:15 +0800

    remove assets

--- public/_snowpack/pkg/@heroicons/vue.v1.0.1/common/_vue_commonjs-external-f2330f44.js
+++
@@ -1,9 +0,0 @@
-import * as vue from '/_snowpack/pkg/vue.v3.0.11.js';
-
-function getDefaultExportFromNamespaceIfNotNamed (n) {
-	return n && Object.prototype.hasOwnProperty.call(n, 'default') && Object.keys(n).length === 1 ? n['default'] : n;
-}
-
-var require$$0 = /*@__PURE__*/getDefaultExportFromNamespaceIfNotNamed(vue);
-
-export { require$$0 as r };
--- public/_snowpack/pkg/has.v1.0.3.js
+++
@@ -1,5 +0,0 @@
-import bind from '/_snowpack/pkg/function-bind.v1.1.1.js';
-
-var src = bind.call(Function.call, Object.prototype.hasOwnProperty);
-
-export default src;
